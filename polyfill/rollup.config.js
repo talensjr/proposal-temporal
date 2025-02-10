b23034ca760f1a2eef05b4b45b5a43717a789815
Jordi Talens <talensjr@gmail.com>
2025-02-10 17:47:57 +0100

    chore: add for publish to production

--- polyfill/rollup.config.js
+++ polyfill/rollup.config.js
@@ -1,24 +1,13 @@
 import commonjs from '@rollup/plugin-commonjs';
-import nodePolyfills from 'rollup-plugin-node-polyfills';
 import resolve from '@rollup/plugin-node-resolve';
-import babel from '@rollup/plugin-babel';
 import replace from '@rollup/plugin-replace';
+import terser from '@rollup/plugin-terser';
 import { env } from 'process';
+import nodePolyfills from 'rollup-plugin-node-polyfills';
 
 const isTest262 = !!env.TEST262;
 const libName = 'temporal';
-const babelConfig = {
-  exclude: 'node_modules/**',
-  babelHelpers: 'bundled',
-  presets: [
-    [
-      '@babel/preset-env',
-      {
-        targets: '> 0.25%, not dead'
-      }
-    ]
-  ]
-};
+
 const replaceConfig = { exclude: 'node_modules/**', preventAssignment: true };
 const resolveConfig = { preferBuiltins: false };
 
@@ -29,7 +18,16 @@ export default [
       replace({ ...replaceConfig, __debug__: false, __isTest262__: isTest262 }),
       commonjs(),
       nodePolyfills(),
-      resolve(resolveConfig)
+      resolve(resolveConfig),
+      terser({
+        compress: {
+          passes: 2
+        },
+        format: {
+          comments: false,
+          ascii_only: true
+        }
+      })
     ],
     output: {
       name: libName,
@@ -37,21 +35,5 @@ export default [
       format: 'iife',
       sourcemap: true
     }
-  },
-  !isTest262 && {
-    input: 'lib/shim.mjs',
-    output: {
-      name: libName,
-      file: '../out/docs/playground.js',
-      format: 'umd',
-      sourcemap: true
-    },
-    plugins: [
-      replace({ ...replaceConfig, __debug__: true, __isTest262__: false }),
-      commonjs(),
-      nodePolyfills(),
-      resolve(resolveConfig),
-      babel(babelConfig)
-    ]
   }
-].filter(Boolean);
+];
